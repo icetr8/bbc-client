f1dd638155223e0b6e9f67383bf253dca37c5acf
angelo suinan <suinanangelo@gmail.com>
2017-12-24 20:20:10 +0800

    add extract message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,19 @@ import static spark.Spark.port;
 import static spark.Spark.post;
 import static spark.Spark.halt;
 
-
-
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
- 
+
+import javax.servlet.http.Part;
+
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import static spark.Spark.delete;
 import static spark.Spark.path;
@@ -27,7 +34,7 @@ public class Main {
         		String mode = request.queryParams("hub.mode");
         		String token = request.queryParams("hub.verify_token");
         		String challenge = request.queryParams("hub.challenge");
-        		if (mode != null && token!= null) {
+        		if (mode!=null && token!= null) {
         			if(mode.equals("subscribe")) {
     				 // Responds with the challenge token from the request
     			      System.out.println("Webhook Verified");
@@ -45,14 +52,28 @@ public class Main {
             	ObjectMapper mapper = new ObjectMapper();
             	Map<String,Object> map = mapper.readValue(body, Map.class);
             	String page = (String) map.get("object");
-        		if (page.equalsIgnoreCase("page"))
-        			System.out.println("ASDASD");
+        		if (page.equalsIgnoreCase("page")) {
+        			String message = getMessage(request.body());
+        			System.out.println(message);
+        		}
         		else
         			halt(404);
             	return "EVENT_RECEIVED";
             	
             });
         });
+    }
+    static String getMessage(String body) {
+    	JsonElement jelement = new JsonParser().parse(body);
+		JsonObject entryobject = jelement.getAsJsonObject();
+	    JsonArray entryarray = entryobject.getAsJsonArray("entry");
+	    JsonObject messagingobject = entryarray.get(0).getAsJsonObject();
+	    JsonArray messagingarray = messagingobject.getAsJsonArray("messaging");
+	    JsonObject messageobject = messagingarray.get(0).getAsJsonObject();
+	    JsonObject message = messageobject.getAsJsonObject("message");
+	    String result = message.get("text").getAsString();
+		return result;
+    	
     }
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
